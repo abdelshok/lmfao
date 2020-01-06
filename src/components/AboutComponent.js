676e56aa851d8fa062cbd11235f56527de70a6be
abdelshok <aas698@nyu.edu>
2020-01-05 21:30:00 -0500

    Fix UX flow of About section

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -11,7 +11,7 @@ import Terminal from 'terminal-in-react';
 const initialMsg = () => "LMFAO is the only (original) chat app for the terminal.\n\nIt is human-focused. LMFAO doesn't record any of your conversations and allows you to message friends and coworkers effortlessly.\n\nIt is secure. LMFAO protects user privacy by using end to end encryption to prevent anyone (us included) from tracking user data.\n\nMore importantly, it is fun. Fun with a capital F. You can join public channels or create private channels to talk with anyone while giving the impression that you are doing intense engineering work.\n\nType 'lmfao' in order to see a preview of the application.";
 const lastMsg = () => "Scroll down to learn more about LMFAO's features"
 const lmfaoMsg = () => "LMFAO \n \n 🙊 \n \n 🙉 \n \n 🙈 \n \n \nYour one and only chat app for the terminal. Please type in 'npm' for a link to learn more about this package or scroll down.\n"
-const npmMsg = () => "You can learn more about LMFAO at npmjs.com/package/lmfao.\n\nPlease type 'open' to open the page in another tab.";
+const npmMsg = () => "You can learn more about LMFAO at npmjs.com/package/lmfao.\n\nType ':)' to come back to the terminal prompt.";
 
 class AboutComponent extends Component {
 
@@ -23,7 +23,7 @@ class AboutComponent extends Component {
     }
 
     
-    lmfaoTyped = () => {
+    getLMFAO = () => {
         setTimeout(() => {
             this.setState({
                 commandTyped: ''
@@ -36,7 +36,7 @@ class AboutComponent extends Component {
         }, 0)
     }
 
-    npmTyped = () => {
+    getNpmInfo = () => {
         setTimeout(() => {
             this.setState({
                 commandTyped: ''
@@ -47,6 +47,12 @@ class AboutComponent extends Component {
                 })
             }, 0)
         }, 0)
+    }
+
+    getInitialInfo = () => {
+        this.setState({
+            commandTyped: 'initialPrompt'
+        })
     }
 
     render() {
@@ -63,7 +69,7 @@ class AboutComponent extends Component {
                 barColor='black'
                 style={{ fontWeight: "bold", fontSize: "1em", height: "60%", textAlign: 'justify'}} // Overrides the parent style
                 commands={{
-                    'lmfao': this.lmfaoTyped,
+                    'lmfao': this.getLMFAO,
                 }}
                 descriptions={{
                     'lmfao': 'Runs the initial prompt of the package',
@@ -83,8 +89,8 @@ class AboutComponent extends Component {
                     barColor='black'
                     style={{ fontWeight: "bold", fontSize: "1em", height: "60%", textAlign: 'justify'}} // Overrides the parent style
                     commands={{
-                        'npm': this.npmTyped, 
-                        'lmfao': this.lmfaoTyped,
+                        'npm': this.getNpmInfo, 
+                        'lmfao': this.getLMFAO,
                     }}
                     descriptions={{
                         showmsg: 'shows a message',
@@ -106,9 +112,10 @@ class AboutComponent extends Component {
                 barColor='black'
                 style={{ fontWeight: "bold", fontSize: "1em", height: "60%", textAlign: 'justify'}} // Overrides the parent style
                 commands={{
-                    'lmfao': this.lmfaoTyped,
-                    'npm': this.npmTyped,
+                    'lmfao': this.getLMFAO,
+                    'npm': this.getNpmInfo,
                     'open':() => window.open('https://www.npmjs.com/package/lmfao'),
+                    ':)': this.getInitialInfo,
 
                 }}
                 descriptions={{
